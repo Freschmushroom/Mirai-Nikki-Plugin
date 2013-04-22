71e258667bdf048998bebb2cfde1a3276a95e706
zaboing <zaboing@gmx.at>
2013-04-22 13:16:02 +0200

    First sketch of the plugin
    No functionality implemented yet.

---
+++ src/at/freschmushroom/bukkit/mirai_nikki/MiraiNikkiPlugin.java
@@ -0,0 +1,13 @@
+package at.freschmushroom.bukkit.mirai_nikki;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MiraiNikkiPlugin extends JavaPlugin {
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
